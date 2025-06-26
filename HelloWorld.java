f88c62a7cab4091d58f8e714790c69d44dbd72c4
KarimLabidProf <karim.labidi@esprit.tn>
2025-06-26 12:14:03 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 class HelloWorld { 
 public static void main(String[] args){ 
-     System.out.println("Hello, World! sur Github"); 
+     System.out.println("Version 2 du JOB BUILD"); 
+     System.out.println("Hello, World! sur Gi"); 
 }  
 }   
